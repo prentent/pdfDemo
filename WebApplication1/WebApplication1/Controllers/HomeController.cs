89ec75c01506ff0dadd891880ec9935bf607a840
2236066560@qq.com <lh3141592654lh>
2019-12-01 15:43:05 +0800

    初次提交

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //所有
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //单页
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
